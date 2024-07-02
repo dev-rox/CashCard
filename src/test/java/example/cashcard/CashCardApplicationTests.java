3bbb2804b7b35de46929fab6e13a27f59dbac672
rox-dev <142692001+dev-rox@users.noreply.github.com>
2024-07-01 23:30:20 -0300

    feat: Create GET endpoint that returns a CashCard of a certain ID

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -1,13 +1,49 @@
 package example.cashcard;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+//This will start our Spring Boot application and make it available for our test to perform requests to it.
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashCardApplicationTests {
 
+    // Asking Spring to inject a test helper that’ll allow us to make HTTP requests to the locally running application.
+    @Autowired
+    TestRestTemplate restTemplate;
+
     @Test
-    void contextLoads() {
+    void shouldReturnACashCardWhenDataIsSaved() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
+        //Here we use restTemplate to make an HTTP GET request to our application endpoint /cashcards/99
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        //This converts the response String into a JSON-aware object with lots of helper methods.
+        Number id = documentContext.read("$.id");
+        assertThat(id).isEqualTo(99);
+
+        Double amount = documentContext.read("$.amount");
+        assertThat(amount).isEqualTo(123.45);
+        /*
+           We expect that when we request a Cash Card with id of 99 a JSON object will be returned with something in the
+           id field.
+        */
     }
 
+    @Test
+    void shouldNotReturnACashCardWithAnUnknownId() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(response.getBody()).isBlank();
+    }
 }
